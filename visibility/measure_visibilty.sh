fce519238c68356b86eb84bc7e5ff19ce9b76d24
cesine <cesine@yahoo.com>
2015-08-12 08:43:05 -0400

    reducing logging

--- visibility/measure_visibilty.sh
+++ visibility/measure_visibilty.sh
@@ -11,14 +11,14 @@ LRL_GITHUB_REPOSITORIES=`cat data/lrl_repositories_github.txt`
 echo "$LRL_GITHUB_REPOSITORIES";
 
 for repo in $LRL_GITHUB_REPOSITORIES; do
-  echo "curl $GITHUB_API/$repo";
+#  echo "curl $GITHUB_API/$repo";
   json=`curl $GITHUB_API/$repo?access_token=$GITHUB_API_TOKEN`;
   echo "$repo"
   echo  " $json" | grep "stargazers_count" ;
-  echo  " $json" | grep "watchers_count" ;
-  echo  " $json" | grep "forks_count" ;
-  echo  " $json" | grep "subscribers_count" ;
-  echo  " $json" | grep "open_issues_count" ;
+#  echo  " $json" | grep "watchers_count" ;
+#  echo  " $json" | grep "forks_count" ;
+#  echo  " $json" | grep "subscribers_count" ;
+#  echo  " $json" | grep "open_issues_count" ;
   echo "$json" > "visibility/results/${repo/\//___}.json"
 done
 

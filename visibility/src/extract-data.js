68ed33f54907bc92598e7f323bd13f7ba9178d4c
cesine <cesine@yahoo.com>
2015-09-17 17:00:00 -0400

    extracting rev ids from the repo to itterate over versions of the json

--- visibility/src/extract-data.js
+++ visibility/src/extract-data.js
@@ -1,5 +1,39 @@
-var extractData = function() {
+var shellPromise = require("./shellPromises").execute;
+var Q = require("q");
 
+
+var extractResultFromJsonAtRevision = function(revision) {
+  console.log("Working on " + revision);
+};
+
+var extractData = function(branchName) {
+  var deferred = Q.defer();
+
+  var findGitRevisisons = "git rev-list " + branchName;
+  shellPromise(findGitRevisisons).then(function(results) {
+    console.log(results);
+    if (!results) {
+      deferred.resolve(results);
+      return;
+    }
+    results = results.trim().split("\n");
+
+    results.map(function(revision) {
+      if (revision) {
+        extractResultFromJsonAtRevision(revision);
+      }
+    });
+
+    deferred.resolve(results);
+  }, function(reason) {
+    console.log(reason);
+    deferred.reject(reason);
+  }).fail(function(exception) {
+    console.log(exception);
+    deferred.reject(exception);
+  });
+
+  return deferred.promise;
 };
 
 exports.extractData = extractData;
--- visibility/test/extract-data-spec.js
+++ visibility/test/extract-data-spec.js
@@ -1,9 +1,29 @@
 var extractData = require("./../src/extract-data").extractData;
+var specIsRunningTooLong = 5000;
 
 describe("extract-data", function() {
 
   it("should load", function() {
     expect(extractData).toBeDefined();
+  });
+
+  describe("pipeline", function() {
+
+    it("loop through the revisions", function(done) {
+      extractData("experiment/improving-visibility").then(function(results) {
+        expect(results).toBeDefined();
+        expect(results.length).toEqual(75);
+        expect(results[results.length-1]).toEqual("5c33b4f4764fec18b9345b21466d42b5a0c55a99");
+      
+
+      },function(results) {
+        expect(results).toBeDefined();
+      }).fail(function(results) {
+        expect(results).toBeDefined();
+      }).done(done);
+
+    }, specIsRunningTooLong);
+
   });
 
 });

86f8a0c9071026166c13788872d7992a71b45e6e
cesine <cesine@yahoo.com>
2015-09-17 16:31:27 -0400

    using a wrapper for shell commands to switch different git commits

---
+++ visibility/src/shellPromises.js
@@ -0,0 +1,30 @@
+var Q = require("q");
+var childProcess = require("child_process");
+var serverInternalPath = new RegExp(__dirname.replace(/lib$/, "") + "[^.]*/", "g");
+
+exports.execute = function(command) {
+	var deferred,
+		localProcess;
+
+	console.log(command);
+	deferred = Q.defer();
+	console.log(new Date() + " PATH", process.env.PATH);
+
+	localProcess = childProcess.exec(command, function(error, stdout, stderr) {
+		// console.log("in result childProcess");
+		if (error !== null) {
+			console.log("");
+			console.log("");
+			console.log("");
+			console.log("");
+			console.log("rejecting childProcess error");
+			console.log(error);
+			deferred.reject(error.message.replace(serverInternalPath, ""));
+		} else {
+			console.log("resolving childProcess stdout");
+			deferred.resolve(stdout);
+		}
+	});
+
+	return deferred.promise;
+};

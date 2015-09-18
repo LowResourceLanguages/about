3d1eac0cd90170d80e8b206825af22ad6ebd9354
cesine <cesine@yahoo.com>
2015-09-18 11:26:52 -0400

    extract file contents

--- visibility/src/extract-data.js
+++ visibility/src/extract-data.js
@@ -1,11 +1,30 @@
 var shellPromise = require("./shellPromises").execute;
 var Q = require("q");
+var fs = require("fs");
 
 var data = {
   measurements: 0
 };
 var parametersToExtract = ["name", "size", "stargazers_count", "watchers_count", "open_issues_count", "forks"];
 var oldestRelevantRevision = "3c5b5e5f6f0faac1fe3d04ed3158acc92a9b1cd4";
+
+var processFile = function(timestamp, filepath) {
+  var deferred = Q.defer();
+
+  console.log("reading " + filepath);
+  fs.readAsync(filepath, function(error, repositoryJson) {
+    if (error) {
+      console.log(" couldn't read this file " + filepath)
+      deferred.reject(error);
+      return;
+    }
+    console.log(" found contents of " + repositoryJson);
+    data[timestamp] = repositoryJson;
+    deferred.resolve(filepath);
+  });
+
+  return deferred.promise;
+};
 
 var extractResultFromJsonAtRevision = function(revision) {
   console.log("Working on " + revision);
@@ -34,7 +53,7 @@ var extractResultFromJsonAtRevision = function(revision) {
   return timestampPromise;
 };
 
-var extractData = function(branchName) {
+var prepareDataStructure = function(branchName) {
   var deferred = Q.defer();
 
   var findGitRevisisons = "git rev-list " + branchName;
@@ -56,7 +75,7 @@ var extractData = function(branchName) {
         promises.push(extractResultFromJsonAtRevision(revision));
       }
     });
-    
+
     console.log("waiting on " + promises.length + " results");
     Q.allSettled(promises).then(function(promiseResults) {
       deferred.resolve(data);
@@ -73,4 +92,36 @@ var extractData = function(branchName) {
   return deferred.promise;
 };
 
+var extractData = function(dirname) {
+  var deferred = Q.defer();
+
+  fs.readdir(dirname, function(error, filelist) {
+    if (error || !filelist) {
+      deferred.reject(error);
+      return;
+    }
+    var promises = [];
+    // console.log("There are these files ", filelist);
+    filelist.map(function(filename) {
+      if (promises.length > 3) {
+        return;
+      }
+      filename = dirname + "/" + filename;
+      console.log(" Working on " + filename);
+      var jsonPromise = processFile(0, filename);
+      promises.push(jsonPromise);
+
+      Q.allSettled(promises).then(function() {
+        deferred.resolve(data);
+      });
+
+      count++;
+    });
+
+  });
+  return deferred.promise;
+
+};
+
+exports.prepareDataStructure = prepareDataStructure;
 exports.extractData = extractData;
--- visibility/test/extract-data-spec.js
+++ visibility/test/extract-data-spec.js
@@ -1,19 +1,33 @@
 var extractData = require("./../src/extract-data").extractData;
+var prepareDataStructure = require("./../src/extract-data").prepareDataStructure;
 var specIsRunningTooLong = 5000;
 
 describe("extract-data", function() {
 
   it("should load", function() {
     expect(extractData).toBeDefined();
+    expect(prepareDataStructure).toBeDefined();
   });
 
-  describe("pipeline", function() {
+  describe("pieces", function() {
 
-    it("loop through the revisions", function(done) {
-      extractData("experiment/improving-visibility").then(function(results) {
+    xit("should loop through the revisions", function(done) {
+      prepareDataStructure("experiment/improving-visibility").then(function(results) {
         expect(results).toBeDefined();
         expect(results.measurements).toEqual(57);
         expect(results[1442523600000]).toBeDefined();
+
+      },function(results) {
+        expect(results).toBeDefined();
+      }).fail(function(results) {
+        expect(results).toBeDefined();
+      }).done(done);
+
+    }, specIsRunningTooLong);
+
+    it("should loop through the files", function(done) {
+      extractData("results").then(function(results) {
+        expect(results).toBeDefined();
 
       },function(results) {
         expect(results).toBeDefined();

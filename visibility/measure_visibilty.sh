62c469ad01748f10932cadbc36db2fbce5341f96
cesine <cesine@yahoo.com>
2015-09-19 18:24:55 -0400

    measures for Wed Aug 12 03:28:19 EDT 2015 combined

--- visibility/measure_visibilty.sh
+++ visibility/measure_visibilty.sh
@@ -4,9 +4,9 @@
 #
 #
 #
-#
+cd $LOW_RESOURCE_LANGUAGES_ABOUT_HOME
 GITHUB_API="https://api.github.com/repos"
-LRL_GITHUB_REPOSITORIES=`cat ../data/lrl_repositories_github.txt`
+LRL_GITHUB_REPOSITORIES=`cat data/lrl_repositories_github.txt`
 
 echo "$LRL_GITHUB_REPOSITORIES";
 
@@ -19,7 +19,11 @@ for repo in $LRL_GITHUB_REPOSITORIES; do
   echo  " $json" | grep "forks_count" ;
   echo  " $json" | grep "subscribers_count" ;
   echo  " $json" | grep "open_issues_count" ;
-  echo "$json" > "results/${repo/\//___}.json"
+  echo "$json" > "visibility/results/${repo/\//___}.json"
 done
 
-ls -al results
+#ls -al visibility/results
+echo `date` >> visibility/results/run.log
+git add visibility/results
+git commit -m "measures for `date`"
+
